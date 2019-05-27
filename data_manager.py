bb82bc60c804014efc1c559438dcf8ddc636cb46
Taras Katrych <KatrychTaras@gmail.com>
2019-05-27 12:00:25 +0200

    implement get key

--- data_manager.py
+++ data_manager.py
@@ -1,14 +1,20 @@
 import csv
 
 FIELDNAMES = ['id', 'submission_time', 'vote_number', 'question_id', 'message,image']
-
-def export_data(filename='sample_data/answer.csv', data_to_write):
+LAST_ELEMENT = -1
+def export_data(filename='sample_data/answer.csv', data_to_write=[]):
     with open(filename, 'w') as f:
         writer = csv.DictWriter(fieldnames=FIELDNAMES)
         writer.writerow(data_to_write)
+
 
 def import_data(filename='sample_data/answer.csv'):
     with open(filename, 'r') as f:
         reader = csv.DictReader(f)
         return [{k:v for k, v in row.items()} for row in reader]
-print(import_data())
+
+
+def get_dictionary_key(key='id', id=LAST_ELEMENT):
+    all_stories = import_data()
+    return 0 if all_stories == [] else all_stories[id][key]
+print(get_dictionary_key('id', 0))
