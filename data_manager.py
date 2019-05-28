961f817bb53dddd5fe70922d3b027da1a1519995
JC Kolodziej <kuba@kolodziej.com.pl>
2019-05-28 10:52:55 +0200

    add add view function

--- data_manager.py
+++ data_manager.py
@@ -2,80 +2,29 @@ import csv
 from datetime import datetime
 
 FIELDNAMES = ['id', 'submission_time', 'vote_number', 'question_id', 'message,image']
-<<<<<<< HEAD
-LAST_ELEMENT = -1
-
-=======
 
 LAST_ELEMENT = -1
+
 
 file = 'sample_data/question.csv'
->>>>>>> d53d6d160a37a570406f185d85679f4d8c1753ac
 
 
-def export_data(filename='sample_data/answer.csv', data_to_write=[]):
+def export_data(filename, data_to_write, fields):
     with open(filename, 'w') as f:
-        writer = csv.DictWriter(fieldnames=FIELDNAMES)
-        writer.writerow(data_to_write)
+        writer = csv.DictWriter(f, fieldnames=fields)
+        writer.writeheader()
+        writer.writerows(data_to_write)
 
 
-def import_data(filename='sample_data/answer.csv'):
+def import_data(filename):
     with open(filename, 'r') as f:
         reader = csv.DictReader(f)
         return [{k:v for k, v in row.items()} for row in reader]
 
 
-def get_dictionary_key(id=LAST_ELEMENT, key='id'):
-    all_stories = import_data()
-<<<<<<< HEAD
-    return 0 if all_stories == [] else all_stories[LAST_ELEMENT][key]
-
-
-
-=======
-    return 0 if all_stories == [] else all_stories[id][key]
-
-
-print(get_dictionary_key('id', 0))
-
-print(import_data())
->>>>>>> d53d6d160a37a570406f185d85679f4d8c1753ac
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def get_dictionary_key(id_=LAST_ELEMENT, key='id'):
+    all_stories = import_data(file)
+    return 0 if all_stories == [] else all_stories[id_][key]
 
 
 def convert_time_from_csv(timestamp):
@@ -84,20 +33,15 @@ def convert_time_from_csv(timestamp):
 
 def get_real_time():
     now = datetime.now()
-<<<<<<< HEAD
-
-=======
     return datetime.timestamp(now)
 
 
 def add_view_count(question_id):
+    fields = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
     key = 'view_number'
-    view_count = get_dictionary_key(key, question_id)
+    view_count = int(get_dictionary_key(question_id, key))
     view_count += 1
     questions_data = import_data(file)
     questions_data[question_id][key] = view_count
-    export_data(file, questions_data)
+    export_data(file, questions_data, fields)
 
-
-add_view_count(1)
->>>>>>> d53d6d160a37a570406f185d85679f4d8c1753ac
