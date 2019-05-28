77381ec2f0b4a198d634a4522fbf53f594711237
Taras Katrych <KatrychTaras@gmail.com>
2019-05-28 10:12:26 +0200

    update get key

--- data_manager.py
+++ data_manager.py
@@ -2,11 +2,10 @@ import csv
 from datetime import datetime
 
 FIELDNAMES = ['id', 'submission_time', 'vote_number', 'question_id', 'message,image']
-<<<<<<< Updated upstream
 LAST_ELEMENT = -1
-=======
 
->>>>>>> Stashed changes
+
+
 def export_data(filename='sample_data/answer.csv', data_to_write=[]):
     with open(filename, 'w') as f:
         writer = csv.DictWriter(fieldnames=FIELDNAMES)
@@ -17,15 +16,14 @@ def import_data(filename='sample_data/answer.csv'):
     with open(filename, 'r') as f:
         reader = csv.DictReader(f)
         return [{k:v for k, v in row.items()} for row in reader]
-<<<<<<< Updated upstream
 
 
-def get_dictionary_key(key='id', id=LAST_ELEMENT):
+def get_dictionary_key(id=LAST_ELEMENT, key='id'):
     all_stories = import_data()
-    return 0 if all_stories == [] else all_stories[id][key]
-print(get_dictionary_key('id', 0))
-=======
-print(import_data())
+    return 0 if all_stories == [] else all_stories[LAST_ELEMENT][key]
+
+
+
 
 
 
@@ -69,5 +67,4 @@ def convert_time_from_csv(timestamp):
 
 def get_real_time():
     now = datetime.now()
-    return datetime.timestamp(now)
->>>>>>> Stashed changes
+
